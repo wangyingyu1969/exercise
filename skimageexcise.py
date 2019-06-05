72bf4b66132398884bf73ce96e62d6e2e8349793
my_paper <1766876122@qq.com>
2019-06-05 21:06:14 +0800

    rountin exercise

---
+++ skimageexcise.py
@@ -0,0 +1,8 @@
+import skimage
+from skimage import io,transform
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+img=io.imread('C:\\Users\\lenovo\\PycharmProjects\\wangyy\\roi.jpg',as_gray=True)
+
+from
